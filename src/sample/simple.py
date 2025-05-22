623219cff761a549a855b0e25c0cbf22b5adc6d4
Adrian Auer <adrian.auer@dfki.de>
2025-05-22 15:46:38 +0200

    structure

---
+++ src/sample/simple.py
@@ -0,0 +1,11 @@
+def add_one(number):
+    """
+    Adds one to the given number.
+
+    Args:
+            number: The number to which one will be added.
+
+    Returns:
+        number: The result of adding one to the number.
+    """
+    return number + 1
